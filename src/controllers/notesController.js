4686ef77b7ef3c602ad419327eaf8aa5ddf08ccf
Serhii Kravcnenko <sergkrava1983@gmail.com>
2025-09-28 23:22:06 +0300

    hw2/2 node

---
+++ src/controllers/notesController.js
@@ -0,0 +1,21 @@
+import { Note } from './models/note.js';
+import createHttpError from 'http-errors';
+
+// Отримати список усіх нотатків
+export const getNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+// Отримати одну нотатку за id
+export const getNoteById = async (req, res, next) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    next(createHttpError(404, 'Note not found'));
+    return;
+  }
+
+  res.status(200).json(note);
+};
---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,11 @@
+
+import { Router } from "express";
+import { getNotes, getNoteById } from '../controllers/notesController.js';
+
+const router = Router();
+
+router.get('/notes', getNotes);
+
+router.get('/notes/:noteId', getNoteById);
+
+export default router;

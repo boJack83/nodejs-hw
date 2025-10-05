fd0a90ceb68c3920dcb7ef9329810673ef169def
Serhii Kravcnenko <sergkrava1983@gmail.com>
2025-10-05 23:52:10 +0300

    add pagination

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,9 +3,23 @@ import createHttpError from 'http-errors';
 
 // Отримати список усіх нотатків
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+
+  const { page = 1, perPage = 10} = req.query;
+  const skip = (page - 1) * perPage;
+
+  const notesQuery = Note.find();
+
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+  res.status(200).json({ page, perPage, totalNotes, totalPages, notes,});
 };
+
+
+
 
 // Отримати одну нотатку за id
 export const getNoteById = async (req, res, next) => {

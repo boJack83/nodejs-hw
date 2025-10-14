49209148b847cd9031d01c3fc3b1d1b2781d34eb
Serhii Kravcnenko <sergkrava1983@gmail.com>
2025-10-14 23:44:35 +0300

    add_authenticate

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT ?? 3030; // Використовуємо значення з .env або дефолтний порт 3030
@@ -19,6 +20,7 @@ const PORT = process.env.PORT ?? 3030; // Використовуємо знач�
 app.use(logger);         // 1. Логер першим — бачить усі запити
 app.use(express.json()); // 2. Парсинг JSON-тіла
 app.use(cors());         // 3. Дозвіл для запитів з інших доменів
+app.use(cookieParser());
 
 app.use(notesRoutes);    // 4. Підключаємо групу маршрутів
 app.use(authRoutes);

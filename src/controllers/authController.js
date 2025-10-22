1276587e836394da79358d4c86cf3eba2c24dde6
Serhii Kravcnenko <sergkrava1983@gmail.com>
2025-10-22 09:48:15 +0300

    fix sendMail

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from "../models/session.js";
-import { sendMail } from "../utils/sendMail.js";
+import { sendEmail } from "../utils/sendMail.js";
 import jwt from 'jsonwebtoken';
 import handlebars from 'handlebars';
 import path from 'node:path';
@@ -127,7 +127,7 @@ export const requestResetEmail = async (req, res, next) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
